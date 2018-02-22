494bdd1cbb5d9fa2b3063979bfe2b1a4367d5411
Lukas Sivickas <l.sivickas@gmail.com>
2018-02-22 13:03:49 +0200

    Testing Slack integration

--- app/src/main/java/com/latenightpenguin/groupdj/StartActivity.java
+++ app/src/main/java/com/latenightpenguin/groupdj/StartActivity.java
@@ -9,5 +9,6 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        // Test comment for Slack and BitBucket integration
     }
 }
