c5121e9e0bc5aa108b9e339afb966f04a17973eb
Mindaugas <mindaugasdirg@gmail.com>
2018-03-20 15:48:20 +0200

    Implement room creation.

--- app/src/main/java/com/latenightpenguin/groupdj/HostActivity.java
+++ app/src/main/java/com/latenightpenguin/groupdj/HostActivity.java
@@ -9,6 +9,9 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.latenightpenguin.groupdj.NetworkServices.ServerHelper;
+import com.latenightpenguin.groupdj.NetworkServices.ServerRequest;
+
 import org.w3c.dom.Text;
 
 import java.util.Random;
@@ -25,18 +28,26 @@ public class HostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
 
+        Random rand = new Random();
+        int userID = rand.nextInt();
+
+        TextView status = (TextView)findViewById(R.id.tw_RoomId);
+
+        ServerHelper serverHelper = new ServerHelper();
+        serverHelper.createRoom(userID, status);
+
         // Generate room id with uppercase letters and numbers
-        mRoomId = new RandomString(getResources().getInteger(R.integer.room_id_lenght)).nextString();
+        //String userId = new RandomString(getResources().getInteger(R.integer.room_id_lenght)).nextString();
         // Shows a toast message with room id
 
-        Toast.makeText(this, "Room id is :" + mRoomId, Toast.LENGTH_LONG).show();
+        /*Toast.makeText(this, "Room id is :" + mRoomId, Toast.LENGTH_LONG).show();
         try {
             // Sets textview's text to room id
             TextView tw_RoomId = (TextView) findViewById(R.id.tw_RoomId);
             tw_RoomId.setText(mRoomId);
         } catch (Exception e){
             Log.e("HostActivity", e.getMessage());
-        }
+        }*/
         btnAdd = findViewById(R.id.btn_AddSong);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
