ffaa8198a8de7cc961efe051c46b52172fbd8356
Lukas Sivickas <l.sivickas@gmail.com>
2018-03-20 16:45:30 +0200

    when entering the host activity user is authentificated with spotify. ( To test have to have Spotify Premium )

--- app/src/main/java/com/latenightpenguin/groupdj/HostActivity.java
+++ app/src/main/java/com/latenightpenguin/groupdj/HostActivity.java
@@ -1,5 +1,6 @@
 package com.latenightpenguin.groupdj;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,15 +12,30 @@ import android.widget.Toast;
 
 import com.latenightpenguin.groupdj.NetworkServices.ServerHelper;
 import com.latenightpenguin.groupdj.NetworkServices.ServerRequest;
-
-import org.w3c.dom.Text;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
 import java.util.Random;
 
-public class HostActivity extends AppCompatActivity {
+public class HostActivity extends AppCompatActivity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback{
+    // Static variables
     private static final String TAG = HostActivity.class.getSimpleName();
-    // Declaring variables
-    String mRoomId;
+    private static final String CLIENT_ID = "1b02f619aa8142db8cd6d3d9bc3d505e";
+    private static final String REDIRECT_URI = "lnpapp://callback";
+    private static final int REQUEST_CODE = 1337;
+
+    // Obejects
+    private Player mPlayer;
+
     // Declaring view elements
     ImageButton btnAdd;
 
@@ -36,18 +52,6 @@ public class HostActivity extends AppCompatActivity {
         ServerHelper serverHelper = new ServerHelper();
         serverHelper.createRoom(userID, status);
 
-        // Generate room id with uppercase letters and numbers
-        //String userId = new RandomString(getResources().getInteger(R.integer.room_id_lenght)).nextString();
-        // Shows a toast message with room id
-
-        /*Toast.makeText(this, "Room id is :" + mRoomId, Toast.LENGTH_LONG).show();
-        try {
-            // Sets textview's text to room id
-            TextView tw_RoomId = (TextView) findViewById(R.id.tw_RoomId);
-            tw_RoomId.setText(mRoomId);
-        } catch (Exception e){
-            Log.e("HostActivity", e.getMessage());
-        }*/
         btnAdd = findViewById(R.id.btn_AddSong);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,5 +60,96 @@ public class HostActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // AUTHENTIFICATION
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(HostActivity.this);
+                        mPlayer.addNotificationCallback(HostActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Toast.makeText(HostActivity.super.getApplicationContext(), "Could not initialize player", Toast.LENGTH_LONG).show();
+                        Log.e(TAG, "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d(TAG, "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d(TAG, "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d(TAG, "User logged in");
+        Toast.makeText(this, "User logged in", Toast.LENGTH_LONG).show();
+
+        // This is the line that plays a song.
+        mPlayer.playUri(null, "spotify:track:3K4HG9evC7dg3N0R9cYqk4", 0, 0);
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Toast.makeText(this, "User logged out", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error var1) {
+        Toast.makeText(this, "Login failed", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d(TAG, "Temporary error occurred");
+        Toast.makeText(this, "Temporary error occurred", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d(TAG, "Received connection message: " + message);
+        Toast.makeText(this, "Received connection message: " + message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/latenightpenguin/groupdj/StartActivity.java
+++ app/src/main/java/com/latenightpenguin/groupdj/StartActivity.java
@@ -57,17 +57,5 @@ public class StartActivity extends AppCompatActivity {
                 }
             }
         });
-
-        btn_Login = (Button) findViewById(R.id.btn_Login);
-        btn_Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Creates a new intent to LogInActivity and starts it
-                Intent intent = new Intent(StartActivity.super.getApplicationContext(),
-                        LogInActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
